9bf6dda3e4b56f8b7c43843ce8d0b888cdc4479f
cpw <cpw+github@weeksfamily.ca>
2023-07-11 10:37:11 -0400

    Add more gradle stuffs

---
+++ buildSrc/src/main/groovy/net/minecraftforge/forge/tasks/InstallerJson.groovy
@@ -0,0 +1,75 @@
+package net.minecraftforge.forge.tasks
+
+import groovy.json.JsonBuilder
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.provider.SetProperty
+import org.gradle.api.tasks.*
+
+import java.nio.file.Files
+
+abstract class InstallerJson extends DefaultTask {
+    @OutputFile abstract RegularFileProperty getOutput()
+    @InputFiles abstract ConfigurableFileCollection getInput()
+    @Input @Optional abstract SetProperty<String> getPackedDependencies()
+    @Input @Optional final Map<String, Object> libraries = new LinkedHashMap<>()
+    @Input Map<String, Object> json = new LinkedHashMap<>()
+    @InputFile abstract RegularFileProperty getIcon()
+    @Input abstract Property<String> getLauncherJsonName()
+    @Input abstract Property<String> getLogo()
+    @Input abstract Property<String> getMirrors()
+    @Input abstract Property<String> getWelcome()
+
+    InstallerJson() {
+        getLauncherJsonName().convention('/version.json')
+        getLogo().convention('/big_logo.png')
+        getMirrors().convention('https://mirrors.neoforged.net/')
+        getWelcome().convention("Welcome to the simple ${project.name.capitalize()} installer.")
+                
+        getOutput().convention(project.layout.buildDirectory.file('install_profile.json'))
+
+        ['client', 'server'].each { side ->
+            ['slim', 'extra'].each { type ->
+                def tsk = project.tasks.getByName("download${side.capitalize()}${type.capitalize()}")
+                dependsOn(tsk)
+                input.from(tsk.output)
+            }
+            def tsk = project.tasks.getByName("create${side.capitalize()}SRG")
+            dependsOn(tsk)
+            input.from(tsk.output)
+        }
+    }
+
+    @TaskAction
+    protected void exec() {
+        def libs = libraries
+        packedDependencies.get().forEach {
+            def path = Util.getMavenPath(project, it)
+            def dep = Util.getMavenDep(project, it)
+            def file = Util.getMavenFile(project, it)
+
+            libs.put(dep.toString(), [
+                name: dep,
+                downloads: [
+                    artifact: [
+                        path: path,
+                        url: "https://maven.neoforged.net/releases/${path}",
+                        sha1: file.sha1(),
+                        size: file.length()
+                    ]
+                ]
+            ])
+        }
+        json.libraries = libs.values().sort{a,b -> a.name.compareTo(b.name)}
+        json.icon = "data:image/png;base64," + new String(Base64.getEncoder().encode(Files.readAllBytes(icon.get().asFile.toPath())))
+        json.json = launcherJsonName.get()
+        json.logo = logo.get()
+        if (!mirrors.get().isEmpty())
+            json.mirrorList = mirrors.get()
+        json.welcome = welcome.get()
+
+        Files.writeString(output.get().getAsFile().toPath(), new JsonBuilder(json).toPrettyString())
+    }
+}
---
+++ buildSrc/src/main/groovy/net/minecraftforge/forge/tasks/LauncherJson.groovy
@@ -0,0 +1,66 @@
+package net.minecraftforge.forge.tasks
+
+import groovy.json.JsonBuilder
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.SetProperty
+import org.gradle.api.tasks.*
+
+import java.nio.file.Files
+
+import static net.minecraftforge.forge.tasks.Util.getArtifacts
+import static net.minecraftforge.forge.tasks.Util.iso8601Now
+
+abstract class LauncherJson extends DefaultTask {
+    @OutputFile abstract RegularFileProperty getOutput()
+    @InputFiles abstract ConfigurableFileCollection getInput()
+    @Input Map<String, Object> json = new LinkedHashMap<>()
+    @Input @Optional abstract SetProperty<String> getPackedDependencies()
+    
+    @Internal final vanilla = project.project(':mcp').file('build/mcp/downloadJson/version.json')
+    @Internal final timestamp = iso8601Now()
+    @Internal final comment = [
+        "Please do not automate the download and installation of Forge.",
+        "Our efforts are supported by ads from the download page.",
+        "If you MUST automate this, please consider supporting the project through https://www.patreon.com/LexManos/"
+    ]
+    @Internal final id = "${project.rootProject.ext.MC_VERSION}-${project.name}${project.version.substring(project.rootProject.ext.MC_VERSION.length())}"
+
+    LauncherJson() {
+        getOutput().convention(project.layout.buildDirectory.file('version.json'))
+
+        dependsOn('universalJar')
+        getInput().from(project.tasks.universalJar.archiveFile,
+                vanilla)
+    }
+
+    @TaskAction
+    protected void exec() {
+        if (!json.libraries)
+            json.libraries = []
+        def libs = [:]
+        getArtifacts(project, project.configurations.installer, false).each { key, lib -> libs[key] = lib }
+        getArtifacts(project, project.configurations.moduleonly, false).each { key, lib -> libs[key] = lib }
+
+        packedDependencies.get().forEach {
+            def path = Util.getMavenPath(project, it)
+            def key = Util.getMavenDep(project, it)
+            def file = Util.getMavenFile(project, it)
+            
+            libs[key] = [
+                name: key,
+                downloads: [
+                    artifact: [
+                        path: path,
+                        url: "https://maven.neoforged.net/releases/${path}",
+                        sha1: file.sha1(),
+                        size: file.length()
+                    ]
+                ]
+            ]
+        }
+        libs.each { key, lib -> json.libraries.add(lib) }
+        Files.writeString(output.get().asFile.toPath(), new JsonBuilder(json).toPrettyString())
+    }
+}
